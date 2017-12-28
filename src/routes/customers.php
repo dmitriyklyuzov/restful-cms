842a42c82a1b69483f006e503dca65cae23df0fb
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-12-27 21:07:09 -0500

    added customer routes

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -51,4 +51,112 @@
 			echo "{error: {'text': ". $e->getMessage()."}}";
 		}
 	});
+
+	// add customer
+	$app->post('/api/customers/add', function (Request $request, Response $response){
+		// capture form data
+		$firstName = $request->getParam('first_name');
+		$lastName = $request->getParam('last_name');
+		$phone = $request->getParam('phone');
+		$email = $request->getParam('email');
+		$address = $request->getParam('address');
+		$city = $request->getParam('city');
+		$state = $request->getParam('state');
+
+		$sql = "INSERT INTO customers (first_name, last_name, phone, email, address, city, state)
+				VALUES (:first_name, :last_name, :phone, :email, :address, :city, :state)";
+
+		try{
+			$db = new db();
+			$conn = $db->connect();
+			// prepare stmt
+			$stmt = $conn->prepare($sql);
+			// bind params
+			$stmt->bindParam(':first_name', $firstName);
+			$stmt->bindParam(':last_name', $lastName);
+			$stmt->bindParam(':phone', $phone);
+			$stmt->bindParam(':email', $email);
+			$stmt->bindParam(':address', $address);
+			$stmt->bindParam(':city', $city);
+			$stmt->bindParam(':state', $state);
+			// execute stmt
+			$stmt->execute();
+			// echo msg
+			echo "{notice: {'text': 'Customer Added'}}";
+
+			$db = null;
+			$conn = null;
+		}
+		catch(PDOException $e){
+			echo "{error: {'text': ". $e->getMessage()."}}";
+		}
+	});
+
+	// update customer
+	$app->put('/api/customers/update/{id}', function (Request $request, Response $response){
+		// get id
+		$id = $request->getAttribute('id');
+
+		// capture form data
+		$firstName = $request->getParam('first_name');
+		$lastName = $request->getParam('last_name');
+		$phone = $request->getParam('phone');
+		$email = $request->getParam('email');
+		$address = $request->getParam('address');
+		$city = $request->getParam('city');
+		$state = $request->getParam('state');
+
+		$sql = "UPDATE customers
+				SET first_name = :first_name,
+					last_name = :last_name,
+					phone = :phone,
+					email = :email,
+					address = :address,
+					city = :city,
+					state = :state
+				WHERE id = $id";
+
+		try{
+			$db = new db();
+			$conn = $db->connect();
+			// prepare stmt
+			$stmt = $conn->prepare($sql);
+			// bind params
+			$stmt->bindParam(':first_name', $firstName);
+			$stmt->bindParam(':last_name', $lastName);
+			$stmt->bindParam(':phone', $phone);
+			$stmt->bindParam(':email', $email);
+			$stmt->bindParam(':address', $address);
+			$stmt->bindParam(':city', $city);
+			$stmt->bindParam(':state', $state);
+			// execute stmt
+			$stmt->execute();
+			$db = null;
+			$conn = null;
+			echo "{notice: {'text': 'Customer Updated'}}";
+		}
+		catch(PDOException $e){
+			echo "{error: {'text': ". $e->getMessage()."}}";
+		}
+	});
+
+	// delete customer
+	$app->delete('/api/customers/delete/{id}', function (Request $request, Response $response){
+		$id = $request->getAttribute('id');
+		$sql = "DELETE FROM customers WHERE id = $id";
+
+		try{
+			$db = new db();
+			$conn = $db->connect();
+			$stmt = $conn->prepare($sql);
+			$stmt->execute();
+			$db = null;
+			$conn = null;
+			echo "{notice: {'text': 'Customer Deleted'}}";
+		}
+		catch(PDOException $e){
+			echo "{error: {'text': ". $e->getMessage()."}}";
+		}
+	});
+
  ?>
