ecd4451e95117aef2b31567e73afb6eddd41af70
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-12-27 22:24:44 -0500

    added CORS support

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -4,6 +4,19 @@
 
 	$app = new \Slim\App;
 
+	// enable CORS
+	$app->options('/{routes:.+}', function ($request, $response, $args) {
+		return $response;
+	});
+
+	$app->add(function ($req, $res, $next) {
+		$response = $next($req, $res);
+		return $response
+				->withHeader('Access-Control-Allow-Origin', '*')
+				->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+				->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	});
+
 	// get all customers
 	$app->get('/api/customers', function(Request $request, Response $response){
 		// query
